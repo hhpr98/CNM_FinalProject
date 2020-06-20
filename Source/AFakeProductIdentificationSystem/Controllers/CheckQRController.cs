36542c81308dfa105d48120c3fa19e522fefb228
anhoapham2302 <hoaan31071998@gmail.com>
2020-06-21 03:33:53 +0700

    create UI checkQR page by 1612210

---
+++ Source/AFakeProductIdentificationSystem/Controllers/CheckQRController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AFakeProductIdentificationSystem.Models;
+
+
+namespace AFakeProductIdentificationSystem.Controllers
+{
+    public class CheckQRController : Controller
+    {
+        // GET: CheckQR
+        public ActionResult Index()
+        {
+            using (var ProductDB = new FakeRealProductSystemEntities())
+            {
+                var _product = ProductDB.Products.ToList();
+                ViewBag.ListProduct = _product;
+            }
+            return View("Index");
+        }
+    }
+}
