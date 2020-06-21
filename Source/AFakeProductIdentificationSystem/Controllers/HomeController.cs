31e14cbaef0c1b93ee81d325d93a4069dd8e1815
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-06-21 19:31:24 +0700

    delete wallet

--- Source/AFakeProductIdentificationSystem/Controllers/HomeController.cs
+++ Source/AFakeProductIdentificationSystem/Controllers/HomeController.cs
@@ -20,9 +20,9 @@ namespace AFakeProductIdentificationSystem.Controllers
         // không dùng static thì mỗi khi gọi actionResult biến blockChain sẽ bị reset về chain rỗng (1 block gốc)
         private static BlockChain blockChain = new BlockChain(proofOfWorkDifficulty: 2, miningReward: 10);
         private static bool isLoaded = false; // chưa load -> nếu bằng true thì không load lại
-        private static List<string> walletList = new List<string>() { adminAddress, user1Address, user2Address }; // khởi đầu với 3 ví
-        private static List<string> passwordList = new List<string>() { "admin", "user1", "user2" };
-        private static string acc = ""; // tên ví, rỗng là chưa đăng nhập
+        //private static List<string> walletList = new List<string>() { adminAddress, user1Address, user2Address }; // khởi đầu với 3 ví
+        //private static List<string> passwordList = new List<string>() { "admin", "user1", "user2" };
+        //private static string acc = ""; // tên ví, rỗng là chưa đăng nhập
 
         // Load , lúc đầu block chỉ có 1 khối, thêm 1 khối giao dịch (cho user1 và user2)
         private void Load()
