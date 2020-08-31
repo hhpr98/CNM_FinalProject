36cb6471b1c1ef70a47dff614b7f128fefb81c16
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-08-31 20:12:06 +0700

    fix utf8

--- Source/AFakeProductIdentificationSystem/Controllers/GenerateQRController.cs
+++ Source/AFakeProductIdentificationSystem/Controllers/GenerateQRController.cs
@@ -42,8 +42,6 @@ namespace AFakeProductIdentificationSystem.Controllers
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap qrCodeImage = qrCode.GetGraphic(20);
 
-            ViewBag.Message = "Tạo mã QR thành công!";
-
             return View(BitmapToBytes(qrCodeImage));
         }
 
